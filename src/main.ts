0e8a55e4bb07a18a8a2e8533a5a6b83bf51667d1
Oberonru <oberonru@mail.ru>
2022-01-21 16:11:04 +0600

    Swagger connected

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import config from './configuration/config';
 
 async function bootstrap() {
@@ -7,7 +8,17 @@ async function bootstrap() {
   const { host, port } = configuration.http;
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+
+  const options = new DocumentBuilder()
+    .setTitle('Purrweb-app')
+    .setDescription('Api documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swaggerDocs', app, document);
+
+  await app.listen(port, host);
 }
 
 bootstrap();
